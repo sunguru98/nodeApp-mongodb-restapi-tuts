1eadfe61772b57a97cfbbe7b4dbe76eeced5f682
sundeep <sunguru98@yahoo.co.in>
2018-09-23 00:44:30 +0530

    Added a GET/todos test

--- server/tests/post_todos.test.js
+++ server/tests/post_todos.test.js
@@ -3,9 +3,19 @@ let expect = require("expect");
 let {app} = require("./../server");
 let {TodoModel} = require("./../model/TodoListModel");
 
+let dummyTodos = [{
+    noteName:"Hello There"
+},{
+    noteName:"Hi Everyone !"
+},{
+    noteName:"Goodbye ALL of you present !"
+}];
+
 beforeEach(done=>{
-    TodoModel.deleteOne({}).then(()=>done());
-})
+    TodoModel.deleteMany({}).then(()=>{
+        TodoModel.insertMany(dummyTodos)
+    }).then(()=>done());
+}); 
 describe("POST/todos",()=>{
     it("should return a valid object from database when valid data is given",done=>{
         let noteName = "Vanakkam Coimbatore";
@@ -20,7 +30,7 @@ describe("POST/todos",()=>{
                 if(err){
                     return done(err);
                 }
-                TodoModel.find().then(results=>{
+                TodoModel.find({noteName}).then(results=>{
                     expect(results.length).toBe(1);
                     expect(results[0].noteName).toBe(noteName);
                     done();
@@ -38,9 +48,22 @@ describe("POST/todos",()=>{
                     return done(err);
                 }
                 TodoModel.find().then(results=>{
-                    expect(results.length).toBe(0);
+                    expect(results.length).toBe(3);
                     done();
                 }).catch(e=>{done(e);})
             });
-    })
-})
+    });
+});
+
+describe("GET /todos",()=>{
+    it("should return all todo lists",(done)=>{
+        
+        request(app)
+            .get("/todos")
+            .expect(200)
+            .expect(res=>{
+                expect(res.body.results.length).toBe(3) ;
+            })
+            .end(done);
+    });
+});
