a1f8ddc3c37b04c67816a202bf3d7d6e57854572
sundeep <sunguru98@yahoo.co.in>
2018-09-23 23:47:41 +0530

    Added new route for DELETE/todos/:id

--- server/server.js
+++ server/server.js
@@ -37,8 +37,22 @@ app.get("/todos/:id",(request,response)=>{
         }
         response.send({todo});
     }).catch(e=>{
-        return resposne.status(400).send(e);
+        return response.status(400).send(e);
     });
+});
+
+app.delete("/todos/:id",(req,res)=>{
+    let receivedId = req.params.id;
+    if(!ObjectID.isValid(receivedId))
+        return res.status(404).send();
+    TodoModel.findOneAndDelete(receivedId).then(todo=>{
+        if(!todo)
+            return res.status(404).send();
+         res.status(200).send({todo});
+    }).catch(e=>{
+        return response.status(400).send();
+    });
+
 });
 
 app.listen(port,()=>{
